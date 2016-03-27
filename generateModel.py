2f0c7ff17ce6c31079280f6855d4e50abd021e62
Jonathan Karr <jonrkarr@gmail.com>
2016-03-26 22:23:15 -0400

    increasing protein copy number

--- generateModel.py
+++ generateModel.py
@@ -30,7 +30,7 @@ concPi = 5e-3 #[Pi](M)
 concH2O = 55 #[H2O] (M)
 concH = 1.1220e-08 # [H] (M)
 nRnaCopy = 5. #Copy numbers of RNA
-nProtCopy = 100. #copy numbers of proteins
+nProtCopy = 1000. #copy numbers of proteins
 cellCycleLength = 8 * 60 * 60. #(s)
 rnaHalfLife = 5 * 60. #(s)
 
